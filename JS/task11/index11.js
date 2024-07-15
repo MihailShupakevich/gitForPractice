7053bfff70a21ca50b41de074ddc8dcea2e3776d
Mihail Schupakevich <m.schupakevich@nv.dunice.net>
2024-07-15 12:19:44 +0300

    +2

--- JS/task11/index11.js
+++ JS/task11/index11.js
@@ -2,3 +2,18 @@
 
 // Напишите функцию для бинарного поиска, она должна принимать
 // отсортированный массив и элемент который ищем
+
+const arr = [1,2,3,6,7,9,9,10,12,15]
+let binaryFn = (arr1, b) => {
+  if (arr1.length % 2 == 0){
+    while(arr1[arr1.length-1] !== b){
+    for (let i = 0; i < arr1.length-1; i++) {
+        arr1.pop()
+        console.log(arr1);
+
+      
+    }
+  }
+}
+}
+binaryFn(arr,2)
--- JS/task9/index9.js
+++ JS/task9/index9.js
@@ -4,4 +4,24 @@
 // считает количество символов в строке и выводит
 //  *количество символов**символ* 
 
-// Пример aaabbcaff → выведет 4a2b1c1a2f
+// Пример aaabbcaff → выведет 3a2b1c1a2f
+
+
+let count = 1;
+let otherNumber;
+let sum= '';
+let getNumberCharacters = (stroka) => {
+
+  for(let i = 0; i<=stroka.length-1; i++){
+    console.log(stroka[i], stroka[i+1]);
+    if(stroka[i]=== stroka[i+1]){ 
+          count +=1;
+                                } 
+    else {sum += `${count}${stroka[i]}`
+          count =1;
+        }
+    }
+}
+
+getNumberCharacters('helllo')
+console.log(sum);
